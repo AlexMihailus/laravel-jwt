5a347dc32178c8bd72fc461be9f2c57173a3e8d1
Alex Mihailus <mihailus.A.G@gmail.com>
2021-12-01 17:05:40 +0200

    first commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <title>JWT SPA</title>
+</head>
+
+<body class="p-5">
+    <div id="app">
+        @yield('content')
+    </div>
+</body>
+
+</html>
